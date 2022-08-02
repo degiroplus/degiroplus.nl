19de2a2c7cf860025036470b019d0b81048d7056
Damien Deloubes <51909965+DamienDeloubes@users.noreply.github.com>
2022-08-02 23:27:34 +0200

    Squashed commit of the following:
    commit e9c97b9b4659a3707dee6b1942fefdda13deb37a
    Author: Damien Deloubes <51909965+DamienDeloubes@users.noreply.github.com>
    Date:   Tue Aug 2 23:26:24 2022 +0200
    
        init tailwind, prettier and nvm
    
    commit da54c1b663a64e5f008edba9e4a2d5646c43a47f
    Author: Damien Deloubes <51909965+DamienDeloubes@users.noreply.github.com>
    Date:   Tue Aug 2 23:20:29 2022 +0200
    
        init nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => (
+    <main>
+        <h1 className="text-3xl font-bold underline">DegiroPlus</h1>
+    </main>
+)
+
+export default Home
